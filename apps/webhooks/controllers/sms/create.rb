72fbf723c96106ec5bf71dd7fcbcdb6b463abcbe
Alyson Takai <shtakai@gmail.com>
2017-11-29 19:31:36 +0900

    add controller w/test

--- apps/webhooks/controllers/sms/create.rb
+++ apps/webhooks/controllers/sms/create.rb
@@ -2,7 +2,12 @@ module Webhooks::Controllers::Sms
   class Create
     include Webhooks::Action
 
+    expose :twiml
+
     def call(params)
+      @twiml = Twilio::TwiML::MessagingResponse.new
+      @twiml.message body: 'Hello from Hanami!'
+      self.body = @twiml.to_s
     end
   end
 end
